422aa1306fc558f526400623e9920bf8c267c648
Ravi shankar upadhyay <158585103+upadhyayravi023@users.noreply.github.com>
2025-05-12 20:56:18 +0530

    Update authRoutes.js

--- src/authRoutes.js
+++ src/authRoutes.js
@@ -51,7 +51,7 @@ router.post("/login", async (req, res) => {
 
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
-    return res.status(400).json({ message: "Invalid Email or Password" });
+    return res.status(400).json({ message: "Invalid Email or Password>>>>" });
   }
 
   const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: "1h" });
